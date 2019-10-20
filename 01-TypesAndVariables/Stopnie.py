843e4cd7795cd3d03f8de26a7e577d54db3d0db7
KarolJanik-IS <ffaannpl@gmail.com>
2019-10-21 01:36:05 +0200

    stopnie

---
+++ 01-TypesAndVariables/Stopnie.py
@@ -0,0 +1,7 @@
+#29. Napisz program, który dla podanej wartości temperatury wyrażonej w stopniach Celsjusza odczytanej z klawiatury wyznaczy temperaturę w stopniach Fahrenheita oraz Kelvina.
+import math
+celciusz = float(input())
+fahrenheit = (celciusz * 1.8) + 32
+kelvin = celciusz +  273.15
+print(celciusz,"stopni celcjusza to ", fahrenheit, "stopni fahrenheita lub ", kelvin, "kelwinów")
+
