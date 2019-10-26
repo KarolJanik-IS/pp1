e5ea0ce56f3b06b1ccb915a7e6a0dc53afc9dec1
KarolJanik-IS <ffaannpl@gmail.com>
2019-10-27 01:19:07 +0200

    zadanko 49 uwzglednia poczatek od dnia 6 z 02

--- 02-ControlStructures/zadanie49.py
+++ 02-ControlStructures/zadanie49.py
@@ -1,20 +1,35 @@
 #49
-nrDniaTygodnia = 2
+nrDniaTygodnia = 3
 i = 1
 j = 0
-row = 0
+ran = 5
 s = ""
 print("| PN | WT | SR | CZ | PT | SB | ND |")
 while i<31:
-    for x in range(0,7):
-        if x == 0:
-            if j<nrDniaTygodnia:
-                s = s+"|    "
+    if nrDniaTygodnia == 6:
+        ran = 6
+    for x in range(0,ran):
+        for y in range(0,7):
+            if x == 0:
+                if y<nrDniaTygodnia:
+                    s = s+"|    "
+                else:
+                    #s = s+"| "+str(i)+" "
+                    if i<10:
+                        s = s+"|  "+str(i)+" "
+                    else:
+                        s = s+"| "+str(i)+" "
+                    i += 1
             else:
-                s = s+"| "+str(i)+" "
-        else:
-            s = s+"| "+str(i)+" "
-        i += 1
+                if i<10:
+                    s = s+"|  "+str(i)+" "
+                else:
+                    if i<31:
+                        s = s+"| "+str(i)+" "
+                    else:
+                        s = s+"|    "
+                i += 1
+            
         s = s+"|"
         print(s)
         s = ""
